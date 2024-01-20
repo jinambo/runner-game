5484b74b02143fb75e7ee5d2c37e7c63e91df242
Jiří Šrytr <jinambo@seznam.cz>
2024-01-20 15:33:15 +0100

    throw kunai if player is close

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public GameObject kunaiPrefab; 
+    public GameObject kunaiPrefab;
+    public GameObject playerPrefab; 
     public Transform kunaiSpawnPoint;
     public GameObject charModel;
+    public float activationDistance = 50.0f;
 
     private float throwInterval = 2.0f;
     private float timer;
@@ -18,14 +21,18 @@ public class EnemyAttack : MonoBehaviour
 
     void Update()
     {
+        float distanceToPlayer = Vector3.Distance(playerPrefab.transform.position, transform.position);
+        if (distanceToPlayer > activationDistance) return;
+
         timer -= Time.deltaTime;
 
         if (timer <= 0)
         {
+            kunaiPrefab.SetActive(true);
             ThrowKunai();
+            timer = throwInterval;
             // Play animation
             charModel.GetComponent<Animator>().Play("Frisbee Throw");
-            timer = throwInterval;
         }
     }
 
